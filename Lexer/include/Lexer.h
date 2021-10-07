f34e266bc4f42a9c80cd1954e92829d27c486d16
Direktor799 <priceclaptrap@hotmail.com>
2021-10-07 23:45:53 +0800

    ✨ feat: add ErrorInfo

--- Lexer/include/Lexer.h
+++ Lexer/include/Lexer.h
@@ -8,24 +8,22 @@
 #include <algorithm>
 
 const std::set<std::string> keyWord = {"auto", "short", "int", "long",
-                         "float", "double", "char", "struct",
-                         "union", "enum", "typedef", "const",
-                         "unsigned", "signed", "extern", "register",
-                         "static", "volatile", "void", "if",
-                         "else", "switch", "case", "for",
-                         "do", "while", "goto", "continue",
-                         "break", "default", "sizeof", "return"};
+                                       "float", "double", "char", "struct",
+                                       "union", "enum", "typedef", "const",
+                                       "unsigned", "signed", "extern", "register",
+                                       "static", "volatile", "void", "if",
+                                       "else", "switch", "case", "for",
+                                       "do", "while", "goto", "continue",
+                                       "break", "default", "sizeof", "return"};
 
 class Token
 {
 private:
     std::string m_type;
     std::string m_value;
+
 public:
-    Token(std::string type, std::string value) : m_type(type), m_value(value)
-    {
-        
-    };
+    Token(std::string type, std::string value) : m_type(type), m_value(value){};
     friend std::ostream &operator<<(std::ostream &out, Token &token)
     {
         out << '<' << token.m_type << ", " << token.m_value << '>';
@@ -33,19 +31,33 @@ public:
     }
 };
 
+class ErrorInfo
+{
+public:
+    size_t m_line;
+    size_t m_col;
+    std::string m_error;
+    ErrorInfo(size_t line, size_t col, std::string error) : m_line(line), m_col(col), m_error(error){};
+};
+
 class Lexer
 {
 private:
+    std::string filePath;
     std::string program;
     std::string currentWord;
     char currentChar;
     size_t pos;
+    size_t line;
+    size_t col;
     std::vector<Token> tokens;
+    std::vector<ErrorInfo> errorinfos;
     void nextChar();
     void rollBack(size_t length = 1);
 
 public:
-    Lexer(std::string filePath);
+    Lexer(std::string path);
     void analyze();
     void outputTokens();
+    void outputErrorInfos();
 };
--- Lexer/src/Lexer.cpp
+++ Lexer/src/Lexer.cpp
@@ -1,7 +1,8 @@
 #include "Lexer.h"
 
-Lexer::Lexer(std::string filePath) : pos(0)
+Lexer::Lexer(std::string path) : pos(0), line(1), col(1)
 {
+    filePath = path;
     std::fstream file(filePath);
     if (!file.is_open())
     {
@@ -20,6 +21,13 @@ void Lexer::nextChar()
         currentChar = EOF;
     else
     {
+        if (currentChar == '\n')
+        {
+            line++;
+            col = 1;
+        }
+        else
+            col++;
         currentWord += program[pos++];
         currentChar = currentWord.back();
     }
@@ -36,26 +44,28 @@ void Lexer::analyze()
 {
     while (true)
     {
+        currentWord.clear();
         nextChar();
-        //std::cout << int(currentChar) << std::endl;
         if (currentChar == EOF)
             break;
-        else if (!isprint(currentChar)) //跳过空格
+        if (!isprint(currentChar)) //跳过空格
         {
             continue;
         }
-        else if (currentChar == '#') //跳过宏
+        if (currentChar == '#') //跳过宏
         {
             while (currentChar != '\n')
                 nextChar();
+            continue;
         }
-        else if (currentChar == '/')  //跳过注释
+        if (currentChar == '/') //跳过注释
         {
             nextChar();
             if (currentChar == '/')
             {
                 while (currentChar != '\n')
                     nextChar();
+                continue;
             }
             else if (currentChar == '*')
             {
@@ -71,32 +81,56 @@ void Lexer::analyze()
                             rollBack();
                     }
                 }
+                continue;
+            }
+            else    //可能是除号
+            {
+                rollBack();
             }
         }
-        else if (isalpha(currentChar) || currentChar == '_') //标识符
+        if (isalpha(currentChar) || currentChar == '_') //标识符
         {
             while (isalnum(currentChar) || currentChar == '_')
                 nextChar();
             rollBack();
             if (keyWord.find(currentWord) != keyWord.end())
-                tokens.emplace_back(Token("KeyWord", currentWord));
+                tokens.emplace_back(Token(currentWord, currentWord));
             else
-                tokens.emplace_back(Token("ID", currentWord));
+                tokens.emplace_back(Token("id", currentWord));
         }
-        else if (isalnum(currentChar)) //数字常量
+        else if (isdigit(currentChar) || isdigit(currentChar)) //数字常量
         {
+            if (currentChar == '0') //bin, oct, hex
+            {
+                nextChar();
+                if (currentChar == 'b' || currentChar == 'B')
+                {
+                    while (currentChar == '0' || currentChar == '1')
+                        nextChar();
+                        int a = 0x11ull;
+                }
+                else if (currentChar == 'x' || currentChar == 'X')
+                {
 
+                }
+                else
+                {
+                    
+                }
+            }
+            else    //dec, float
+            {
+
+            }
         }
         else if (currentChar == '\'') //字符常量
         {
             nextChar();
             if (currentChar == '\\')
             {
-
             }
             else
             {
-
             }
         }
         else if (currentChar == '\"') //字符串常量
@@ -105,13 +139,132 @@ void Lexer::analyze()
         }
         else if (currentChar == '<')
         {
-            /* code */
+            nextChar();
+            if (currentChar != '=')
+                rollBack();
+            tokens.emplace_back(Token("rel_op", currentWord));
         }
         else if (currentChar == '>')
         {
-            /* code */
+            nextChar();
+            if (currentChar != '=')
+                rollBack();
+            tokens.emplace_back(Token("rel_op", currentWord));
         }
-        currentWord.clear();
+        else if (currentChar == '=')
+        {
+            nextChar();
+            if (currentChar == '=')
+                tokens.emplace_back(Token("rel_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("assign_op", currentWord));
+            }
+        }
+        else if (currentChar == '!')
+        {
+            nextChar();
+            if (currentChar == '=')
+                tokens.emplace_back(Token("rel_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("not_op", currentWord));
+            }
+        }
+        else if (currentChar == '+')
+        {
+            nextChar();
+            if (currentChar == '=')
+                tokens.emplace_back(Token("plus_assign_op", currentWord));
+            else if (currentChar == '+')
+                tokens.emplace_back(Token("inc_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("plus_op", currentWord));
+            }
+        }
+        else if (currentChar == '-')
+        {
+            nextChar();
+            if (currentChar == '=')
+                tokens.emplace_back(Token("sub_assign_op", currentWord));
+            else if (currentChar == '-')
+                tokens.emplace_back(Token("dec_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("sub_op", currentWord));
+            }
+        }
+        else if (currentChar == '*')
+        {
+            nextChar();
+            if (currentChar == '=')
+                tokens.emplace_back(Token("mul_assign_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("mul_op", currentWord));
+            }
+        }
+        else if (currentChar == '/')
+        {
+            nextChar();
+            if (currentChar == '=')
+                tokens.emplace_back(Token("div_assign_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("div_op", currentWord));
+            }
+        }
+        else if (currentChar == '|')
+        {
+            nextChar();
+            if (currentChar == '|')
+                tokens.emplace_back(Token("rel_op", currentWord));
+            else if (currentChar == '=')
+                tokens.emplace_back(Token("or_assign_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("or_op", currentWord));
+            }
+        }
+        else if (currentChar == '&')
+        {
+            nextChar();
+            if (currentChar == '&')
+                tokens.emplace_back(Token("rel_op", currentWord));
+            else if (currentChar == '=')
+                tokens.emplace_back(Token("and_assign_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("and_op", currentWord));
+            }
+        }
+        else if (currentChar == '^')
+        {
+            nextChar();
+            if (currentChar == '=')
+                tokens.emplace_back(Token("xor_assign_op", currentWord));
+            else
+            {
+                rollBack();
+                tokens.emplace_back(Token("xor_op", currentWord));
+            }
+        }
+        else if (currentChar == '(' || currentChar == ')' ||
+        currentChar == '{' || currentChar == '}' ||
+        currentChar == '[' || currentChar == ']' ||
+        currentChar == ';')
+        {
+            tokens.emplace_back(Token("sep", currentWord));
+        }
     }
 }
 
@@ -119,4 +272,13 @@ void Lexer::outputTokens()
 {
     for (auto &token : tokens)
         std::cout << token << std::endl;
+}
+
+void Lexer::outputErrorInfos()
+{
+    for (auto &errorinfo : errorinfos)
+    {
+        std::cout << filePath << ':' << errorinfo.m_line << ':' << errorinfo.m_col << ": error:";
+        std::cout << errorinfo.m_error << std::endl;
+    }
 }
